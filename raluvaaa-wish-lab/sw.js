98ff860bd2059a696c19ade654a7b8818f87a3d2
thomasjudes-del <thomas.judes@gmail.com>
2026-09-12 00:50:28 +0200

    Cache RALUVAAA wish lab packaged build

--- raluvaaa-wish-lab/sw.js
+++ raluvaaa-wish-lab/sw.js
@@ -1,5 +1,5 @@
-const CACHE='raluvaaa-wish-test-v1';
-const ASSETS=['./','./index.html','./manifest.webmanifest'];
+const CACHE='raluvaaa-wish-test-v2';
+const ASSETS=['./','./index.html','./app.html.gz','./manifest.webmanifest'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
 self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r}).catch(()=>caches.match(e.request)))})
